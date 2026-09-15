de098f9e640fd37de0d7b4bcbceae489e24bc880
Ingeniero Luis Grisales <contactoluiancol@gmail.com>
2026-09-15 11:43:29 -0500

    Update Supabase anon key in configuration

--- js/config/supabase.js
+++ js/config/supabase.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = 'https://rkehgfvjnenklygxgvqy.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzbWF4b2Zpcm9sendiaWdmeGlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc4Mzc3MjcsImV4cCI6MjEwMzQxMzcyN30.f08TAG5vLZTkd880RLi2gzaYO_CtRzmamNFcjBtzS30';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrZWhnZnZqbmVua2x5Z3hndnF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0MjY1MTQsImV4cCI6MjEwNDAwMjUxNH0.GOy8YoyCaB16-LzFcQmtEpuKrOeKrMq2jM6h6eHiF-s';
 
 export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
